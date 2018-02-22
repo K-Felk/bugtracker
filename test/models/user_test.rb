b7593bdf90cf423e772831e2ec656a42a87e24de
Sean Driscoll <driscols@mail.gvsu.edu>
2018-02-22 16:34:12 -0500

    final commit

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @bug = bug(:one)
+    @user = user(:one)
+  end
+  
+  test "user must be valid" do
+    assert @user.valid?
+  end
+  
+  test "first name must be present" do
+    @user.fname = ""
+    assert_not @user.valid?
+  end
+  
+  test "last name must be present" do
+    @user.lname = ""
+    assert_not @user.valid?
+  end
+  
+  test "email must be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+  
+  test "email must be valid" do
+    @user.email = "test"
+    assert_not @user.valid?
+  end
+  
+  test "email is valid" do
+    @user.email = "email@email.com"
+    assert @user.valid?
+  end
 end
